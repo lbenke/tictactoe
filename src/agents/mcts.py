5979e85014c95286fa50dab54286c3e289c0ab17
lbenke <lyndon.benke@gmail.com>
2018-01-09 18:08:08 +1100

    MCTS agent working (fully random, no UCT yet)

--- src/agents/mcts.py
+++ src/agents/mcts.py
@@ -7,8 +7,7 @@ import rules
 import time
 import random
 import graphing.mcts_graph as graphing
-
-DEBUG = False
+import itertools
 
 
 class MCTSAgent(Player):
@@ -26,14 +25,12 @@ class MCTSAgent(Player):
 
     def mcts(self, board):
         current_player = self.side
-        iterations_max = 1000
+        iterations_max = 200
         iterations = 0
 
         root_node = TreeNode(board)
 
         while iterations < iterations_max:  # change to time and/or move to inner loop
-            if DEBUG: print "Iteration", iterations, "initial state:"
-
             iterations += 1
 
             # Pick tree root (current actual state)
@@ -42,7 +39,6 @@ class MCTSAgent(Player):
 
             terminal_state = False
             while not terminal_state:
-                if DEBUG: print rules.board_str(current_node.state), "\n"
                 # Pick a random move
                 empty_cells = rules.empty_cells(current_node.state)
                 move = tuple(random.choice(empty_cells))
@@ -62,9 +58,7 @@ class MCTSAgent(Player):
                 current_player = -current_player
 
             # Terminal state reached so backpropagate result
-            if DEBUG: print "Terminal state:\n", rules.board_str(current_node.state)
             winner = rules.winner(current_node.state)
-            if DEBUG: print "\nBackpropagating...\n"
             while current_node is not root_node:
                 current_node.visits += 1
                 if winner == self.side:
@@ -74,24 +68,20 @@ class MCTSAgent(Player):
                 # Move up the tree
                 current_node = current_node.parent
 
-            if DEBUG: print "------------------------------\n"
-
-        if DEBUG: print "Visits and score for each child move tested:\n"
-        for child in current_node.child_nodes.values():
-            if DEBUG: print rules.board_str(child.state), "Visits", child.visits, "| Score", child.score, "\n"
+        # Visualise the tree
+        graphing.graph_mcts_tree(current_node)
 
         # Return move with highest score
         best_move = root_node.best_move()
-        if DEBUG: print "\nBest move:", best_move
-
-        # Visualise the tree
-        graphing.graph_mcts_tree(current_node)
 
         return best_move
 
 
 class TreeNode(object):
+    new_id = itertools.count().next
+
     def __init__(self, board, parent=None):
+        self.id = TreeNode.new_id()
         self.state = board
         self.parent = parent
         self.visits = 0;
@@ -101,8 +91,6 @@ class TreeNode(object):
     def best_move(self):
         best_move = None
         for move in self.child_nodes:
-            if DEBUG: print round(self.child_nodes[move].ratio(), 4)
-
             if (best_move is None or self.child_nodes[move].ratio() >
                     self.child_nodes[best_move].ratio()):
                 best_move = move
@@ -110,33 +98,40 @@ class TreeNode(object):
         return best_move
 
     def ratio(self):
-        return float(self.score) / float(self.visits)
+        if self.visits != 0:
+            return float(self.score) / float(self.visits)
+        else:
+            return None
+
+    def to_string(self):
+        return rules.board_str(self.state)
 
 
 if __name__ == "__main__":
-    # import numpy as np
-    # mcts_agent = MCTSAgent()
-    # mcts_agent.side = rules.NOUGHT
-    # #board = np.asarray([[-1, 1, 1], [0, -1, 1], [-1, -1, 0]])
-    # #board = np.asarray([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+    import numpy as np
+    mcts_agent = MCTSAgent()
+    mcts_agent.side = rules.CROSS
+    # board = np.asarray([[-1, 1, 1], [0, -1, 1], [-1, -1, 0]])
+    # board = np.asarray([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
     # board = np.asarray([[-1, 0, -1], [0, 0, 0], [0, 0, 0]])
-    # mcts_agent.move(board)
+    board = np.asarray([[-1, 1, 1], [0, -1, 0], [0, 1, 0]])
+    mcts_agent.move(board)
 
-    import logging
-    import sys
-    from tictactoe import TicTacToe
-    # Set up the logger
-    logger = logging.getLogger()
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO,
-        format="\n%(message)s")
-
-    # Create the players
-    from players import Human
-    human = Human(logger=logger)
-    from agents.mcts import MCTSAgent
-    agent = MCTSAgent(logger=logger)
-
-    # Run the game
-    game = TicTacToe([human, agent], shuffle=True, logger=logger)
-    while True:
-        game.run()
+    # import logging
+    # import sys
+    # from tictactoe import TicTacToe
+    # # Set up the logger
+    # logger = logging.getLogger()
+    # logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+    #     format="\n%(message)s")
+    #
+    # # Create the players
+    # from players import Human
+    # human = Human(logger=logger)
+    # from agents.mcts import MCTSAgent
+    # agent = MCTSAgent(logger=logger)
+    #
+    # # Run the game
+    # game = TicTacToe([human, agent], shuffle=True, logger=logger)
+    # while True:
+    #     game.run()
